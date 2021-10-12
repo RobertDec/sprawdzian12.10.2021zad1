63b302f61d45c214f9835c255d8efa4fcaa3cd36
Robert <87870041+RobertDec@users.noreply.github.com>
2021-10-12 14:22:38 +0200

    Add files via upload

---
+++ przetwarzanie plikow 1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    fstream plik("wsp.txt", ios::in);
+    fstream wynik("wynik.txt", ios::out);
+    int x, y;
+    while (!plik.eof() == true){
+        plik >> x >> y;
+        if (x > 0 && y > 0) {
+            wynik << x << " " << y << " I Cwiartka" << endl;
+        }
+        else if(x<0&&y>0){
+            wynik << x << " " << y << " II Cwiartka" << endl;
+
+        }
+        else if (x < 0 && y<0) {
+            wynik << x << " " << y << " III Cwiartka" << endl;
+
+        }
+        else {
+            wynik << x << " " << y << " IV Cwiartka" << endl;
+
+        }
+
+    }
+    return 0;
+}
